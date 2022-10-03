4f93abb2e2b14c56347cb1a3105906945d1616d8
zcratx <zcratx@gmail.com>
2022-10-03 12:59:50 -0700

    The page is getting displayed and major milestone is reached

--- src/main/java/springexample/NormalizeController.java
+++ src/main/java/springexample/NormalizeController.java
@@ -1,5 +1,7 @@
 package springexample;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -22,9 +24,22 @@ public class NormalizeController {
        //load the file post.json
         File configFile = new File("post.json");
         String jsonFileContent = "";
+        String jsonFileContentBeautified = "";
+        Object json = null;
 
         if(configFile.exists() && !configFile.isDirectory()) {
             jsonFileContent = PPCPVisualizerFactory.fromConfigFile(configFile);
+        }
+
+        // loading the Jackson Object mapper to beautify the JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            json = objectMapper.readValue(jsonFileContent, Object.class);
+            jsonFileContentBeautified = objectMapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(json);
+        } catch(Exception jpe) {
+            System.out.println("Problem converting JSON text to JSON Object");
+            jsonFileContentBeautified = jsonFileContent;
         }
 
         // load the file report.html
@@ -35,7 +50,7 @@ public class NormalizeController {
             reportFileContent = PPCPVisualizerFactory.fromConfigFile(reportFile);
         }
 
-        model.addAttribute("JSONFILE", jsonFileContent);
+        model.addAttribute("JSONFILE", jsonFileContentBeautified);
 
         model.addAttribute("REPORTFILE", reportFileContent);
 
